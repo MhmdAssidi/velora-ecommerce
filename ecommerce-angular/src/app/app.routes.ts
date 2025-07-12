513ee6ad8d4df7fa21cd97737a02ee270d9633b0
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-12 23:08:41 +0300

    feat: create forgot password page

--- ecommerce-angular/src/app/app.routes.ts
+++ ecommerce-angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/login/signup/signup.component';
 import { LoginComponent } from './pages/login/login/login.component';
+import { ForgotPasswordComponent } from './pages/login/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
    { path: 'signup', component: SignupComponent },
    { path: 'signin', component: LoginComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: '', redirectTo: 'signup', pathMatch: 'full' },
 ];
---
+++ ecommerce-angular/src/app/pages/login/forgot-password/forgot-password.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../../../shared/components/button/button.component";
+@Component({
+  selector: 'app-forgot-password',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ButtonComponent
+  ],
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.scss'
+})
+export class ForgotPasswordComponent  implements OnInit {
+ forgotPasswordForm!: FormGroup;  //signinForm is the form.
+
+  constructor(private fb: FormBuilder) {}
+
+ngOnInit(): void {
+  this.forgotPasswordForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', Validators.required],
+
+  });
+}
+
+onSubmit(): void {
+  if (this.forgotPasswordForm.invalid) {
+    this.forgotPasswordForm.markAllAsTouched();
+    return;
+  }
+
+  console.log(this.forgotPasswordForm.value);
+}
+}
