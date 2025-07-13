9b30613034b0f8a022eed81b0f620eaa9298efe2
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-14 01:57:02 +0300

    feat: add AuthGuard and GuestGuard to protect routes

--- ecommerce-angular/src/app/app.routes.ts
+++ ecommerce-angular/src/app/app.routes.ts
@@ -4,12 +4,28 @@ import { LoginComponent } from './pages/login/login/login.component';
 import { ForgotPasswordComponent } from './pages/login/forgot-password/forgot-password.component';
 import { ConfirmationWindowComponent } from './pages/login/confirmation-window/confirmation-window.component';
 import { NewPasswordComponent } from './pages/login/new-password/new-password.component';
+import { GuestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
-   { path: 'signup', component: SignupComponent },
-   { path: 'signin', component: LoginComponent },
+   { path: 'signup', component: SignupComponent,canActivate:[GuestGuard]},
+   { path: 'signin', component: LoginComponent,canActivate: [GuestGuard]},
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'confirmation', component: ConfirmationWindowComponent },
       { path: 'new-password', component: NewPasswordComponent },
+  //     {
+  //   path: 'shop',
+  //   component: ShopComponent,
+  //   canActivate: [AuthGuard]
+  // },
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent,
+  //   canActivate: [AuthGuard]
+  // },
+  // {
+  //   path: 'checkout',
+  //   component: ProfileComponent,
+  //   canActivate: [AuthGuard]
+  // }
     { path: '', redirectTo: 'signup', pathMatch: 'full' },
 ];
