7fde2d41d77ae4e0734bd672f0101d633a67cfd3
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-18 11:23:50 +0300

    Add onBtnClicked to shared button to make signup action

--- ecommerce-angular/src/app/shared/components/button/button.component.ts
+++ ecommerce-angular/src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,5 +18,6 @@ export class ButtonComponent {
   @Input() appearance: string = 'primary';
   @Input() imgIcon: string | null = null;
   @Input() customClass: string | null = null;
+  @Output() onBtnClicked: EventEmitter<any> = new EventEmitter;
 
 }
