c09724039c1cc20958a99fe8be5fd57e8d2d12e9
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-22 01:25:29 +0300

    feat: add Deals section, starting of its style

---
+++ ecommerce-angular/src/app/pages/home/deals-section/deals-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/components/button/button.component";
+
+@Component({
+  selector: 'app-deals-section',
+  imports: [ButtonComponent],
+  templateUrl: './deals-section.component.html',
+  styleUrl: './deals-section.component.scss'
+})
+export class DealsSectionComponent {
+
+}
