1116ddfdb5ae756b68e037c6b129439ddb67d4d9
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-22 22:01:09 +0300

    check login if work and add console log to check the response of it

--- ecommerce-angular/src/app/core/auth/authentication.service.ts
+++ ecommerce-angular/src/app/core/auth/authentication.service.ts
@@ -13,6 +13,8 @@ export class AuthenticationService {
 //we need to store the token returned from backend api
 
 setToken(token: string): void {  //saves the token returned by the backend into localStorage.used in login action
+   console.log('Setting token:', token);  // This should log the token to the console
+
   localStorage.setItem(this.tokenKey, token);
 }
 
@@ -27,14 +29,15 @@ setToken(token: string): void {  //saves the token returned by the backend into 
   login(credentials: { email: string, password: string }): Observable<any> { 
 
     //first send pass and email which are the credentails to the backend api using http
-  return this.http.post<any>('http://192.168.7.156:5005/api/User/Login()', credentials).pipe(
-    tap(response => { //tap lets you “do something” after getting a response. response here is what we will get from api 
-      if (response && response.token) { //if response return a token
-        this.setToken(response.token); //store it in local storage
-      }
-    })
-  );
-}
+    return this.http.post<any>('http://192.168.7.156:5005/api/User/Login()', credentials).pipe(
+      tap(response => { //tap lets you “do something” after getting a response. response here is what we will get from api 
+        console.log('API Response:', response);  // Log the response
+        if (response && response.token) { //if response return a token
+          this.setToken(response.token); //store it in local storage
+        }
+      })
+    );
+  }
 signup(data: {
   Firstname: string;
   Lastname: string;
@@ -44,6 +47,7 @@ signup(data: {
 }): Observable<any> {
   return this.http.post<any>('http://192.168.7.156:5005/api/User/SignUp()', data).pipe(
     tap(response => {
+      console.log('API Response:', response);  // Log the response
       if (response && response.token) {
         this.setToken(response.token);
       }
