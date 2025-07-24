b0d1009d7f8f986e8e853f71f20c45f13e88c477
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-07-24 17:38:35 +0300

    Update navbar to show logout if user is logged in

--- ecommerce-angular/src/app/layout/header/header.component.ts
+++ ecommerce-angular/src/app/layout/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../shared/components/button/button.component";
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../core/auth/authentication.service';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonComponent,RouterModule],
+  imports: [ButtonComponent,RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(public authService: AuthenticationService,private router: Router) {}
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
 
 }
