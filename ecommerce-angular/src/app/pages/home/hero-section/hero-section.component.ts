8d308c7c64eb404509f8a88be74d8dbd6e17c9ad
Mohammad Assidi <mhmdassidi2002@gmail.com>
2025-08-06 14:03:57 +0300

    chnage the style of cards in shop and home page

--- ecommerce-angular/src/app/pages/home/hero-section/hero-section.component.ts
+++ ecommerce-angular/src/app/pages/home/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../shared/components/button/button.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,RouterModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
--- ecommerce-angular/src/app/pages/home/home.component.ts
+++ ecommerce-angular/src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { BrandsSectionComponent } from "./brands-section/brands-section.componen
 import { DealsSectionComponent } from "./deals-section/deals-section.component";
 import { InstagramSectionComponent } from "../../shared/components/instagram-section/instagram-section.component";
 import { TrendingProductsComponent } from "./trending-products/trending-products.component";
+import { FindFashionSectionComponent } from "../../shared/components/find-fashion-section/find-fashion-section.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSectionComponent, BrandsSectionComponent, DealsSectionComponent, InstagramSectionComponent, TrendingProductsComponent],
+  imports: [HeroSectionComponent, BrandsSectionComponent, DealsSectionComponent, InstagramSectionComponent, TrendingProductsComponent, FindFashionSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
