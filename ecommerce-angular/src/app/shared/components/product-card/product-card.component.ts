28b6bbd7faa25b5825d48c434597f99a61d2f205
MohammadAssidi <mhmdassidi2002@gmail.com>
2026-03-27 15:56:08 +0200

    Show the cart icon and activate add to cart functionality even if user is not logged in. Also add the add to cart button to the cards.

--- ecommerce-angular/src/app/shared/components/product-card/product-card.component.ts
+++ ecommerce-angular/src/app/shared/components/product-card/product-card.component.ts
@@ -1,19 +1,35 @@
 import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import { ButtonComponent } from "../button/button.component";
 import { RouterModule } from '@angular/router';
 import { CurrencyPipe, TitleCasePipe, SlicePipe } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatCardModule, ButtonComponent,RouterModule,CurrencyPipe, TitleCasePipe, SlicePipe],
+  imports: [MatCardModule, RouterModule,CurrencyPipe, TitleCasePipe, SlicePipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
+  constructor(private cartService: CartService) {}
+
   @Input() title!: string;
   @Input() desc!: string;
   @Input() image!: string;
   @Input() price!: number;
   @Input() productId!:number;
+
+  addToCart(): void {
+    const product: Product = {
+      id: this.productId,
+      title: this.title,
+      price: this.price,
+      description: this.desc,
+      category: '',
+      image: this.image
+    };
+
+    this.cartService.addToCart(product, 1, '#000', 'M');
+  }
 }
